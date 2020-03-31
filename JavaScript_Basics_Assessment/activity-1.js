3641db3326dca2cd982ef3b89350b3052ad33da3
Vivian W <mvwu@users.noreply.github.com>
2020-03-31 15:18:25 -0500

    added condition to activity-2.js'

---
+++ JavaScript_Basics_Assessment/activity-1.js
@@ -0,0 +1,6 @@
+const product = 9 * 9;
+const quotient = 56 / 12;
+const remainder = 281 % 9;
+const quotient2 = 18 / 0;
+const sum = '56' + '92';
+const compare = (42 === 52);
---
+++ JavaScript_Basics_Assessment/activity-2.js
@@ -0,0 +1,7 @@
+var name = prompt('Enter your name')
+
+if (name.length > 4) {
+    alert('Your name is longer than four characters');
+} else {
+    alert('Your name is less than or equal to four characters');
+}
