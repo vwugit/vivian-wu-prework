fc72d625d0613a4d58ecc0aa6f0c03ff5d63797b
Vivian W <mvwu@users.noreply.github.com>
2020-04-02 13:52:00 -0500

    countdown is counting properly with key presses

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -3,43 +3,38 @@ currentWord = document.querySelector('#currWord');
 guessesRemain = document.querySelector('#guessesLeft');
 wrongGuesses = document.querySelector('#guessedWrong');
 
-var wordList = ['Aladdin', 'Mulan', 'Frozen', 'Hercules', 'Dumbo', 'Cinderella', 'Coco', 'Cars', 'Brave', 
+const wordList = ['Aladdin', 'Mulan', 'Frozen', 'Hercules', 'Dumbo', 'Cinderella', 'Coco', 'Cars', 'Brave',
     'Bambi', 'Pinocchio', 'Fantasia', 'Tarzan', 'Cars', 'Enchanted', 'Bolt', 'Tangled', 'Zootopia', 'Moana'];
-var word = wordList[Math.floor(Math.random() * wordList.length)];
-var guess = [];
-var guessed = [];
-var remainingGuesses = 12;
+let word = wordList[Math.floor(Math.random() * wordList.length)];
+let guess = [];
+let guessed = [];
+let remainingGuesses = 12;
 guessesRemain.innerText = remainingGuesses;
 
-
-var start = function() {
-    for (let i=0; i < word.length; i++){
+let start = function () {
+    for (let i = 0; i < word.length; i++) {
         guess[i] = "_";
     }
-    
-    // displays the letters count without revealing the word
-    currentWord.innerHTML = guess.join(" ");
+    return guess;
 }
 
 start();
 
+// displays the letter progress
+currentWord.innerHTML = guess.join(" ");
 
-
-
-document.addEventListener("keydown", checkKeyPressed);
-
-var checkKeyPressed = function(letter){
-    for (let i=0; i<word.length; i++) {
-        if (letter.keycode == guess[i].charCodeAt(0)){
-            guess[i] = word.split("")[i];
-        } else {
-            remainingGuesses--;
-
-        }
-        guessesRemain.innerText = remainingGuesses;
-        currentWord.innerHTML = guess.join(" ");
+let checkKeyPressed = function (event) {
+    let keyword = String.fromCharCode(event.keycode);
+    console.log(word);
+    if (word.indexOf(keyword) > -1) {
+        console.log(true);
+    } else {
+        remainingGuesses--;
     }
-    if (remainingGuesses < 1) {
-    alert("Nice try, better luck next time.");       
-    }
+    guessesRemain.innerText = remainingGuesses;
 }
+
+document.addEventListener('keypress', checkKeyPressed);
+
+
+
