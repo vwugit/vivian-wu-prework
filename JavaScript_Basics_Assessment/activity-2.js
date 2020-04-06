8a975b2142e5aa70c1daeaa3684eea1e9f954e96
Vivian W <mvwu@users.noreply.github.com>
2020-04-06 13:37:39 -0500

    removed unnecessary semicolons

--- JavaScript_Basics_Assessment/activity-2.js
+++ JavaScript_Basics_Assessment/activity-2.js
@@ -4,4 +4,4 @@ if (name.length > 4) {
     alert('Your name is longer than four characters');
 } else {
     alert('Your name is less than or equal to four characters');
-};
+}
--- JavaScript_Basics_Assessment/activity-3.js
+++ JavaScript_Basics_Assessment/activity-3.js
@@ -3,8 +3,8 @@ var studentNames = ['Anna', 'Bella', 'Cody'];
 for (let i = 0; i < 3; i++) {
     const name = prompt('Please enter a name');
     studentNames.push(name);
-};
+}
 
 for (let i = 0; i < studentNames.length; i++) {
     console.log(studentNames[i]);
-};
+}
